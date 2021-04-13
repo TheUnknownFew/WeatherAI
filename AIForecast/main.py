d3ff5f3ed335a2f7b9836cc3364abebfb91f0f77
Vase of Petunias <TheUnknownFew@users.noreply.github.com>
2021-04-12 22:11:18 -0400

    full merge

--- AIForecast/main.py
+++ AIForecast/main.py
@@ -1,15 +1,15 @@
 # This is the main file
-from AIForecast import sysutils
+from AIForecast import utils
 import tkinter as tk
 
-from AIForecast.weather import dataaccess
-from AIForecast.ui.widgets import AppWindow, Menus, MainMenu, TestMenu, TrainMenu
+from AIForecast.access import WeatherAccess
+from AIForecast.ui.widgets import AppWindow, Menus, MainMenu, TestMenu, TrainMenu, ClimateChangeMenu
 
 
 def main():
-    sysutils.log(__name__).debug('Starting AI-Weather Forecast!')
+    utils.log(__name__).debug('Starting AI-Weather Forecast!')
 
-    dataaccess.load_historical_data()
+    WeatherAccess.load_historical_data()
 
     root = tk.Tk()
     root.title("AI-Weather Forecast")
@@ -19,6 +19,7 @@ def main():
     app.register_menu(MainMenu(app.frame), Menus.MAIN_MENU)
     app.register_menu(TestMenu(app.frame), Menus.TEST_MENU)
     app.register_menu(TrainMenu(app.frame), Menus.TRAIN_MENU)
+    app.register_menu(ClimateChangeMenu(app.frame), Menus.DATA_VIEWER_MENU)
     app.display_screen(Menus.MAIN_MENU)
 
     root.mainloop()
